8cdfe196b14bc357db5797c876b58c0e63cf8264
didskfo <didskfo2003@gmail.com>
2026-03-10 21:34:02 +0900

    [FEAT] WebClient 분리

--- src/main/java/com/example/capstone/global/config/WebClientConfig.java
+++ src/main/java/com/example/capstone/global/config/WebClientConfig.java
@@ -1,14 +1,43 @@
 package com.example.capstone.global.config;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
-    public WebClient webClient() {
+    @Qualifier("defaultWebClient")
+    public WebClient defaultWebClient() {
         return WebClient.builder().build();
+    }
+
+    @Bean
+    @Qualifier("fastApiWebClient")
+    public WebClient fastApiWebClient(
+            @Value("${ai.fastapi.base-url}") String baseUrl,
+            @Value("${ai.fastapi.connect-timeout-millis}") int connectTimeoutMillis,
+            @Value("${ai.fastapi.read-timeout-seconds}}") int readTimeoutSeconds
+    ) {
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, connectTimeoutMillis)
+                .responseTimeout(Duration.ofSeconds(readTimeoutSeconds))
+                .doOnConnected(conn ->
+                        conn.addHandlerLast(new ReadTimeoutHandler(readTimeoutSeconds, TimeUnit.SECONDS)));
+
+        return WebClient.builder()
+                .baseUrl(baseUrl)
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
     }
 }
