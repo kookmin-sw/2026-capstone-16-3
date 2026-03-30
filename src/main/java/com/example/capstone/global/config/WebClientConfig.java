4adc1c0d18edae958406909129521bace2b83b20
didskfo <didskfo2003@gmail.com>
2026-03-30 15:11:23 +0900

    [CHORE] import 수정

--- src/main/java/com/example/capstone/global/config/WebClientConfig.java
+++ src/main/java/com/example/capstone/global/config/WebClientConfig.java
@@ -8,7 +8,6 @@ import io.netty.handler.timeout.ReadTimeoutHandler;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
