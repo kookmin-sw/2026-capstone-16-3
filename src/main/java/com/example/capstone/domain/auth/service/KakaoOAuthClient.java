3edf02837cdfcfafa415516f79e4c95561f29798
didskfo <didskfo2003@gmail.com>
2026-04-07 23:21:00 +0900

    [CHORE] 사용하지 않는 import 문 정리

--- src/main/java/com/example/capstone/domain/auth/service/KakaoOAuthClient.java
+++ src/main/java/com/example/capstone/domain/auth/service/KakaoOAuthClient.java
@@ -1,14 +1,10 @@
 package com.example.capstone.domain.auth.service;
 
-import com.example.capstone.domain.auth.dto.response.KakaoTokenResponse;
 import com.example.capstone.domain.auth.dto.response.KakaoUserResponse;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
