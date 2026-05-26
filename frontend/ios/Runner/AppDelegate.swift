d86e80cfa51f0bc51d18d75f00e7d05e820b9846
jooya38 <hyj0102809@gmail.com>
2026-05-26 15:00:34 +0900

    [FIX] iOS 카카오 로그인 후 앱 복귀 시 인증 완료 안 되는 문제 수정

--- frontend/ios/Runner/AppDelegate.swift
+++ frontend/ios/Runner/AppDelegate.swift
@@ -10,4 +10,15 @@ import UIKit
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+  override func application(
+    _ app: UIApplication,
+    open url: URL,
+    options: [UIApplication.OpenURLOptionsKey: Any] = [:]
+  ) -> Bool {
+    if AuthApi.isKakaoTalkLoginUrl(url) {
+      _ = AuthController.handleOpenUrl(url: url)
+    }
+    return super.application(app, open: url, options: options)
+  }
 }
